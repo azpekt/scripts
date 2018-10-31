d7e12f812f005261d5afa86cb101cbaf47631f5c
nick <azpekt@gmail.com>
2018-10-31 11:24:15 +0000

    added script for k8s users and ns creation

---
+++ k8s_x509_create_user.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+NS=$1
+USERNAME=$2
+
+echo "..creating private key..."
+openssl genrsa -out $USERNAME.pem 2048
+
+echo "..creating CSR..."
+openssl req -new -key $USERNAME.pem -out $USERNAME.csr -subj "/CN=$USERNAME"
+
+echo "..getting encoded CSR..."
+ENCODE=$(cat $USERNAME.csr  | base64 | tr -d '\n')
+
+echo "..creating NS..."
+kubectl create ns $NS
+
+echo "..approving CSR..."
+
+cat <<EOF | kubectl create -f -
+apiVersion: certificates.k8s.io/v1beta1
+kind: CertificateSigningRequest
+metadata:
+  name: csr-$USERNAME
+spec:
+  groups:
+  - system:authenticated
+  request: $ENCODE
+  usages:
+  - digital signature
+  - key encipherment
+  - client auth
+EOF
+kubectl certificate approve csr-$USERNAME
+
+echo "..getting certificate..."
+kubectl get csr csr-$USERNAME  -o jsonpath='{.status.certificate}' | base64 --decode > $USERNAME.crt
+
+echo "..creating and adding contexts...change the cluster name!"
+kubectl config set-credentials $USERNAME --client-key=$USERNAME.pem --client-certificate=$USERNAME.crt
+kubectl config set-context $USERNAME --cluster=kubernetes-the-hard-way --namespace=$NS --user=$USERNAME
+
+echo "..creating rolebinging..."
+kubectl create rolebinding rb-admin-$USERNAME --clusterrole=admin --user=$USERNAME --namespace=$NS
+kubectl config use-context $USERNAME
